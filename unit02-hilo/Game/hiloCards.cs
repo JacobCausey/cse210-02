0a3d0076ca85507ac24e8989703ccb3936ea3d10
Jacob Causey <cau16002@byui.edu>
2022-05-12 20:06:22 -0600

    justcreatingobjectsrn

--- unit02-hilo/Game/hiloCards.cs
+++ unit02-hilo/Game/hiloCards.cs
@@ -7,40 +7,6 @@ namespace unit02_hilo.Game
     // 1) Add the class declaration.
     class Card 
     {
-        public int value = 0;
-        public int points = 0;
-
-    // 2) Create the class constructor. Use the following method comment.
-
-        /// <summary>
-        /// Constructs a new instance of Die.
-        /// </summary>
-    public Card()
-    {
-    }
-    
-    // 3) Create the Roll() method. Use the following method comment.
-    public void Flip()
-    {
-        Random random = new Random();
-        value = random.Next(1, 13);
-
-        if (value == 5)
-        {
-        points = 50;
-        }
-        else if (value == 1)
-        {
-            points = 100;
-        }
-        else
-        {
-            points = 0;
-        }
-    }    
-        /// <summary>
-        /// Generates a new random value and calculates the points for the die. Fives are 
-        /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
-        /// </summary>
+       public int[] cards = {1,2,3,4,5,6,7,8,9,10,11,12,13};
     }     
 }
--- unit02-hilo/Program.cs
+++ unit02-hilo/Program.cs
@@ -1,22 +1,12 @@
-using Game;
+using System;
 
-
-namespace Unit02
+namespace unit02_hilo
 {
-    /// <summary>
-    /// The program's entry point.
-    /// </summary>
     class Program
     {
-        /// <summary>
-        /// Starts the program using the given arguments.
-        /// </summary>
-        /// <param name="args">The given arguments.</param>
-        static int Main(string[] args)
+        static void Main(string[] args)
         {
-            Director director = new Director();
-            director.StartGame();
-            return 0;
+            Console.WriteLine("Hello World!");
         }
     }
 }
