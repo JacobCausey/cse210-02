c06072a83ee9b93e5da2587b75636cb7b5865456
Jacob Causey <cau16002@byui.edu>
2022-05-14 22:50:48 -0600

    Minorsubmission

---
+++ unit02-hilo/Game/PlayerInput.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace unit02_hilo.Game
+{
+    public class PlayerInput
+    {
+        public char cardGuess = ' ';
+    }
+}
--- unit02-hilo/Game/hiloCards.cs
+++ unit02-hilo/Game/hiloCards.cs
@@ -4,35 +4,37 @@ using System;
 namespace unit02_hilo.Game
 {
     
-   class Card
+   public class Card
     {
         public int value = 0;
         public int previousCardValue = 0;
 
-        public string difference = "";
+        public char difference = ' ';
 
         public Card()
     {
     }
+
     public void Flip()
     {  
         Random rand = new Random();
         
         value = rand.Next(0,14);
 
+        // guess = 'h'
+        // value = 4
+        // previousCardValue = 6
+
         if (value > previousCardValue)
         {
-            difference = "h";
+            difference = 'h';
         }
         else if (value < previousCardValue)
         {    
-            difference = "l";
+            difference = 'l';
         }
 
         previousCardValue = value;
-        Console.WriteLine(difference);
-        Console.WriteLine(value);
-        Console.WriteLine(previousCardValue);
     }
     }
 
